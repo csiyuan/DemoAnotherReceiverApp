66e7082dcb84e1a6681858108d3c1afe2619a8cc
Cheng Si Yuan <19016011@rp.edu.sg>
2020-08-20 09:33:20 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rpc346/id19016011/demoanotherreceiverapp/AnotherBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package sg.edu.rpc346.id19016011.demoanotherreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AnotherBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Received in AnotherBroadcastReceiver", Toast.LENGTH_LONG).show();
+    }
+}
